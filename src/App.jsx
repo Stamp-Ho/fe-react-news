2371d6f6f62b9cc77691c543b742b40548c65d2e
inho <inho8024s@gmail.com>
2026-01-12 15:54:06 +0900

    design: 기본 레이아웃 및 font, color 스타일 지정 Co-authored-by: 연승환 <61185991+roony1225@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div>
+          <svg />
+          <a>뉴스스탠드</a>
+        </div>
+        <div>날짜</div>
+      </header>
+      <section>
+        <article>
+          <ul>
+            <li>
+              <a>언론사</a>
+              <a>기사제목</a>
+            </li>
+            <li>
+              <a>언론사</a>
+              <a>기사제목</a>
+            </li>
+          </ul>
+        </article>
+      </section>
+      <section>
+        <header>
+          <div>
+            <a href="">전체언론사</a>
+            <a href="">
+              내가 구독한 언론사<span>0</span>
+            </a>
+          </div>
+          <div>
+            <svg />
+            <svg />
+          </div>
+        </header>
+
+        <div>
+          <svg /> {/* 왼쪽 페이지 */}
+          <div>
+            <button>button</button>
+          </div>
+          <svg /> {/* 오른쪽 페이지 */}
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,70 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // 다크모드 대응을 위해 추가
+  theme: {
+    extend: {
+      colors: {
+        // 하위 팔레트 정의
+        grayscale: {
+          white: "#FFFFFF",
+          "white-alt": "rgba(255, 255, 255, 0.7)",
+          50: "#F5F7F9",
+          100: "#D2DAE0",
+          200: "#879298",
+          300: "#6E8091",
+          400: "#5F6E76",
+          500: "#4B5966",
+          black: "#14212B",
+        },
+        blue: {
+          100: "#7890E7",
+          500: "#4362D0",
+        },
+        // 디자인 토큰 (시맨틱 컬러)
+        text: {
+          strong: "var(--color-text-strong)",
+          bold: "var(--color-text-bold)",
+          default: "var(--color-text-default)",
+          weak: "var(--color-text-weak)",
+          point: "#4362D0", // blue.500
+        },
+        surface: {
+          default: "var(--color-surface-default)",
+          alt: "var(--color-surface-alt)",
+          brand: {
+            default: "#4362D0",
+            alt: "#7890E7",
+          },
+        },
+        border: {
+          default: "var(--color-border-default)",
+          bold: "var(--color-border-bold)",
+        },
+      },
+      borderRadius: {
+        100: "8px",
+        full: "999px",
+      },
+      boxShadow: {
+        popup:
+          "0 4px 2px rgba(20, 33, 43, 0.02), 0 2px 18px rgba(20, 33, 43, 0.08)",
+      }, // tailwind.config.js 의 theme.extend 내부에 추가
+      fontSize: {
+        // display-bold24 -> size 24, bold 700
+        "display-bold24": ["24px", { fontWeight: "700", lineHeight: "auto" }],
+        "display-bold16": ["16px", { fontWeight: "700", lineHeight: "auto" }],
+        "display-bold14": ["14px", { fontWeight: "700", lineHeight: "auto" }],
+        "display-bold12": ["12px", { fontWeight: "700", lineHeight: "auto" }],
+
+        // display-medium16 -> size 16, weight 500, line-height 22px
+        "display-medium16": ["16px", { fontWeight: "500", lineHeight: "22px" }],
+        "display-medium14": ["14px", { fontWeight: "500", lineHeight: "22px" }],
+        "display-medium12": ["12px", { fontWeight: "500", lineHeight: "auto" }],
+      },
+      fontFamily: {
+        sans: ["Pretendard", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+};
